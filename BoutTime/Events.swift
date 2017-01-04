2e363dcbd6f7a65769c95bff171639144785a13b
rrohweder <RogerRohweder@yahoo.com>
2017-01-03 20:58:24 -0500

    Added assets, created draft UI for the Events page.

---
+++ BoutTime/Events.swift
@@ -0,0 +1,19 @@
+//
+//  Events.swift
+//  BoutTime
+//
+//  Created by Roger Rohweder on 12/30/16.
+//  Copyright © 2016 Roger Rohweder. All rights reserved.
+//
+
+import Foundation
+
+class Events {
+    
+    
+    
+    // read event data into collection
+    
+    // return 4 unique events
+    
+}
